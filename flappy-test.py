7d15c4dc8e0b98d8fdd6083479be83fc91f45643
Peter Marangos <lifelessmango@gmail.com>
2018-01-11 00:15:30 -0500

    flappy-test: Add histories

--- flappy-test.py
+++ flappy-test.py
@@ -1,15 +1,32 @@
 import time
 import gym
 import gym_ple
+import tensorflow as tf
+
+PREVIOUS_ACTION_MEMORY = 100
 
 env = gym.make('FlappyBird-v0')
 env.reset()
 
-for _ in range(10000):
+state_history = []
+action_history = []
+reward_history = []
+
+for _ in range(100):
+    action = None
     if _ % 20 ==0:
-        env.step(0)
+        action = 0
     else:
-        env.step(1)
+        action = 1
+    observation, reward, done, info = env.step(action)
+
+    state_history.append(observation)
+    action_history.append(action)
+    reward_history.append(reward)
+    
+    print(observation.shape)
+    # print(env.render(mode='rgb_array').shape)
+    print(done)
+    print(reward)
     env.render(mode='human')
-    # env.render(mode='rgb_array')
-    time.sleep(0.1)
+    time.sleep(0.025)
